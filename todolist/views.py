07a509b6a74e30f2563d1481b4dafc23b560b1d2
syifaafra <afra.mumt@gmail.com>
2022-09-29 11:02:28 +0700

    edit show_todo

--- todolist/views.py
+++ todolist/views.py
@@ -83,11 +83,6 @@ def edit_task(request, pk):
 @login_required(login_url='/todolist/login/')
 def show_todo(request):
     task_list = Task.objects.filter(user=request.user).all()
-    user_todo_list = []
-    current_user = request.user
-    for task in task_list:
-        if task.user == current_user:
-            user_todo_list.append(task)
     context = {
         'task_list': task_list,
     }
